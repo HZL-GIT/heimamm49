56c59c73745d4abb9ae51412403a891529e73134
HZL <you@example.com>
2020-04-05 10:41:27 +0800

    项目起步-elementUI整合

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
-<template></template>
+<template>
+  <div>
+      <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
 
 <script>
 export default {};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入element插件
+import ElementUI from 'element-ui';
+// 导入element响应的css样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 注册element-ui
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
