c90024497fcd3047581795de411ce24e1131ba39
HZL <you@example.com>
2020-04-09 16:22:08 +0800

    加入element布局容器

--- src/router/router.js
+++ src/router/router.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 
 // import Login from '../view/login/login.vue'
 import Login from '@/view/login/login.vue'
+import Layout from '@/view/home/layout.vue'
 
 // 导入路由
 import VouRouter from 'vue-router';
 Vue.use(VouRouter);
 const router = new VouRouter({
     routes: [
-        {path:'/',component:Login}
+        { path: '/', component: Login },
+        { path: '/home', component: Layout }
     ]
 });
 export default router;
---
+++ src/view/home/layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-container class="layout">
+    <el-header class="header">Header</el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.layout {
+  height: 100%;
+  .header {
+    height: 60px;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 2px 5px 0px rgba(63, 63, 63, 0.35);
+  }
+  .aside {
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 2px 5px 0px rgba(63, 63, 63, 0.35);
+  }
+}
+</style>
